008ce11d1bec49bf0aec3626f17afc7e1411526c
Piotr Zebrowski <p.zebrowski@neurosys.com>
2022-04-15 08:51:47 +0200

    settings.py - code for MySQL db restored

--- core/settings.py
+++ core/settings.py
@@ -79,10 +79,17 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+import pymysql
+pymysql.install_as_MySQLdb()
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django-app-db',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'db',
+        'PORT': '3306'
     }
 }
 
